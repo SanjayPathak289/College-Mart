32d03002fa3cf155efafb636ccb404ba283dfa41
Sanjay Pathak <sanjaywrongcode@gmail.com>
2024-03-16 15:40:13 +0530

    check

--- backend/index.js
+++ backend/index.js
@@ -34,9 +34,10 @@ app.use("/api/payment", paymentRoutes);
 // ----------Deployment----------
 const __dirname1 = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '/frontend/frontendmart/build')));
+    console.log(__dirname1);
+    app.use(express.static(path.join(__dirname1, '../frontend/frontendmart/dist')));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "frontend","frontendmart" ,"build", "index.html"));
+        res.sendFile(path.resolve(__dirname1, "frontend", "frontendmart", "dist", "index.html"));
     })
 }
 else {
